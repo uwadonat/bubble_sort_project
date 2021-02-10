c8ba630add1196af4197bc4d25c116398a82dec5
uwadonat <uwadonat@gmail.com>
2021-02-10 18:20:18 +0200

    create bubble_sort_arry

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,2 +1,21 @@
-a = 5
-puts a
+def bubble_sort(arr)
+
+    i=0
+
+    while i < arr.length() do
+
+        if arr[i] > arr[i+1]
+
+            arr[i],arr[i+1]=arr[i+1],arr[i]
+        end
+       i += 1
+    end
+
+      
+
+end
+
+
+
+puts bubble_sort([4,3])
+
