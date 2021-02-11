9b6a0833c9afc0d9e20127ec0443b9ba6c5f43eb
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-02-11 16:58:58 +0500

    fix rubocop errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,18 @@
 def bubble_sort(array)
   n = array.length
   loop do
-  swap = false
-
-    (n - 1).times do |i|
-      if array[i] > array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i]
-        swap = true
+    swap = false
+      (n - 1).times do |i|
+        if array[i] > array[i + 1]
+          array[i], array[i + 1] = array[i + 1], array[i]
+          swap = true
+        end
       end
+
+    break if swap == false
     end
-
-  break if !swap
-  end
-
   array
+  end
 end
 
 puts bubble_sort([4, 3, 78, 2, 0, 2])
