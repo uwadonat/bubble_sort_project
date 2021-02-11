4e61b7fbec20402d124e245c7a73a991eebf61b3
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-02-11 15:37:11 +0500

    UPDATE BUBBLE_SORT_BY.RB

--- bubble_sort_by.rb
+++ bubble_sort_by.rb
@@ -14,6 +14,6 @@ def bubble_sort_by(array)
   print(array)
 end
 
-bubble_sort_by(%w[hi hello hey]) do |left, right|
+bubble_sort_by(["hi", "hello", "hey"]) do |left, right|
   left.length - right.length
 end
