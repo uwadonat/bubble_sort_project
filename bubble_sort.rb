8001819c913f6bf438232da1115da9eca7847e5b
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-02-11 17:08:32 +0500

    fix some linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -2,15 +2,14 @@ def bubble_sort(array)
   n = array.length
   loop do
     swap = false
-      (n - 1).times do |i|
-        if array[i] > array[i + 1]
-          array[i], array[i + 1] = array[i + 1], array[i]
-          swap = true
-        end
+    (n - 1).times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swap = true
       end
-
-    break if swap == false
     end
+    break if swap == false
+  end
   array
 end
 
